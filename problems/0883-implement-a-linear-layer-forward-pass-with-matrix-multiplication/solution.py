bbcc456030b321c7a2ccf66aa6a912d652c08f8c
asuta274 <48102364+asuta274@users.noreply.github.com>
2026-09-17 18:33:08 +0000

    Update: Implement a Linear Layer Forward Pass with Matrix Multiplication

--- problems/0883-implement-a-linear-layer-forward-pass-with-matrix-multiplication/solution.py
+++ problems/0883-implement-a-linear-layer-forward-pass-with-matrix-multiplication/solution.py
@@ -3,8 +3,5 @@ import torch
 def linear_forward(x: torch.Tensor, W: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     # TODO: implement y = x W^T + b using PyTorch ops
     W = W.transpose(0, 1)
-    print(x.shape)
-    print(W.shape)
     result = torch.matmul(x, W) + b
-    print(result)
     return result
